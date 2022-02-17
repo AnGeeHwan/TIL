3da02930d72da96b78ba9fa3b1af3e2bd2ef9340
Gavin <71584396+AnGeeHwan@users.noreply.github.com>
2022-02-17 15:54:51 +0900

    Delete src directory

--- src/pages/TvDetail.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TvDetailPage: React.FC = () => {
-  return <div>TvDetailPage</div>;
-};
-
-export default TvDetailPage;
--- src/pages/TvPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TvPage: React.FC = () => {
-  return <div>tvmain page</div>;
-};
-
-export default TvPage;
