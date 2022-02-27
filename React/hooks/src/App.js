67ccbcc5cf2149cf21d357bf4cb0ceff22620e6b
Gavin <71584396+AnGeeHwan@users.noreply.github.com>
2022-02-27 14:00:44 +0900

    Add files via upload

--- React/hooks/src/App.js
+++ React/hooks/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./Counter";
 import Info from "./Info";
+import Average from "./Average";
 
 function App() {
   return (
     <>
       <Counter />
       <Info />
+      <Average />
     </>
   );
 }
